441b7e8387653f7fd72a73796eb14a21550bcce6
dargyris <diamantisargiris@gmail.com>
2020-11-15 00:25:42 +0100

    Basic stuff.

---
+++ 01_BasicJS/01_HelloWorld.js
@@ -0,0 +1,8 @@
+//alert( "Hello Wrold!" );
+console.log( "Hello World!" );
+let js = "boring";
+if ( js === "amazing!" ){
+    console.log( "JavaScript is FUN!" );
+} else if ( js === "boring" ){
+    console.log( "JavaScript is Boring!" );
+}
